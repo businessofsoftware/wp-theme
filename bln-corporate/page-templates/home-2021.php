1090cc17cceb1a433325a967355090d8b851637b
businessofsoftware <80267145+businessofsoftware@users.noreply.github.com>
2021-03-08 20:19:01 +0000

    Add placeholder for new template

---
+++ bln-corporate/page-templates/home-2021.php
@@ -0,0 +1,13 @@
+<?php
+	/*
+	Template Name: Home 2021
+	*/
+    get_header();
+    get_template_part('content', 'join');
+    get_template_part('content', 'hero');
+    get_template_part('content', 'why');
+    get_template_part('content', 'event-featured-list');
+    get_template_part('content', 'testimonials-list');
+    get_template_part('content', 'home-social');
+    get_footer();
+?>
