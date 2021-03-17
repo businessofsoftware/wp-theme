c9eefe7976bfa3fa7f5980f3665af1770ac47273
businessofsoftware <80267145+businessofsoftware@users.noreply.github.com>
2021-03-17 11:27:53 +0000

    Basic version

--- bln-corporate/page-templates/home-2021.php
+++ bln-corporate/page-templates/home-2021.php
@@ -4,10 +4,30 @@
 	*/
     get_header();
     get_template_part('content', 'join');
-    get_template_part('content', 'hero');
-    get_template_part('content', 'why');
-    get_template_part('content', 'event-featured-list');
-    get_template_part('content', 'testimonials-list');
-    get_template_part('content', 'home-social');
+?>
+
+<style type="text/css">
+    footer {
+        margin-top: -18px;
+    }
+</style>
+
+<div class="container-outer-full-width">
+        
+    <?php
+        while(have_posts()) {
+            the_post();
+            the_content();
+        }
+    ?>
+                            
+</div>
+
+<?php 
+    //get_template_part('content', 'hero');
+    //get_template_part('content', 'why');
+    //get_template_part('content', 'event-featured-list');
+    //get_template_part('content', 'testimonials-list');
+    //get_template_part('content', 'home-social');
     get_footer();
 ?>
