54770b9f2bf793f445e6852152bdd38521c08c10
businessofsoftware <80267145+businessofsoftware@users.noreply.github.com>
2021-03-29 21:45:31 +0100

    Change blog post heading style

--- bln-corporate/content-post.php
+++ bln-corporate/content-post.php
@@ -7,9 +7,9 @@
 
 	<div class="container-outer bk-white">
 
-		<h1 class="bk-0 beak beak-0"><?php the_title(); ?> </h1>
+		<h1 class="bk-0 beak beak-0 heading-2021"><?php the_title(); ?> </h1>
 
-		<div class="container-inner bk-white">
+		<div class="container-inner bk-white post-container-2021">
 
 			<h4 class="dateline dark"><?php the_time('F j, Y'); ?> by <?php the_author('F j, Y'); ?></h4>
 
