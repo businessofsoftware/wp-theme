185aba8d2bcba29b0687c34af6fcf5996f9896b9
businessofsoftware <80267145+businessofsoftware@users.noreply.github.com>
2021-05-13 13:21:57 +0100

    woo commerce single page styles

--- bln-conference/woocommerce.php
+++ bln-conference/woocommerce.php
@@ -4,6 +4,84 @@
 	get_template_part('content', 'register-full-width');
 ?>
 
+<style type="text/css">
+	
+.product .onsale, .product .woocommerce-product-gallery, .product .woocommerce-tabs, .product .related, .product .product_meta {
+	display: none;
+}
+
+.product-thumbnail {
+	display: none;
+}
+
+.product .price {
+	color: black;
+    margin-top: 20px;
+}
+
+.product del {
+	color: gray;
+}
+
+.product h1 {
+	font-size: 36px;
+}
+
+.product h3 {
+	font-size: 24px;
+}
+
+.product h1::before {
+  content: "Purchase: ";
+}
+
+.product .woocommerce-product-details__short-description {
+	margin-bottom: 20px;
+}
+
+.product input.qty {
+	padding: 6px;
+	font-weight: bold;
+}
+
+.product .wc-box-office-ticket-fields {
+	margin-bottom: 40px;
+}
+
+.product button {
+	color: #fff !important;
+    background: rgb(238,94,48);
+    font-size: 1.5em;
+    box-shadow: 2px 3px #000;
+    border: none;
+    border-radius: 1.55em;
+    cursor: pointer;
+    display: inline-block;
+    padding: .667em 1.333em;
+    text-align: center;
+    text-decoration: none;
+    overflow-wrap: break-word;
+}
+
+.woocommerce-notices-wrapper {
+	margin-bottom: 20px;
+}
+
+.woocommerce-notices-wrapper .button {
+	padding: 0.0em 1.0em;
+}
+
+.product .form-row label {
+	width: 110px;
+    display: inline-block;
+}
+
+.woocommerce {
+	margin-top: 40px;
+}
+
+</style>
+
 <section>
 
 	<div class="container-outer bk-white overflow">
