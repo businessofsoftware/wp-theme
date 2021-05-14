c7825389414790a679ba2fd424b1a2d0bda45166
businessofsoftware <80267145+businessofsoftware@users.noreply.github.com>
2021-05-14 18:12:56 +0100

    remove inline style

--- bln-conference/page.php
+++ bln-conference/page.php
@@ -9,7 +9,7 @@
 	<?php get_template_part('content', 'title-block'); ?>
 
 	<div class="container-outer bk-white overflow">
-		<div class="container-inner" style="padding: 0px 40px 40px 40px">
+		<div class="container-inner">
 			<div class="content-main">
 				<?php
 					while(have_posts()) {
