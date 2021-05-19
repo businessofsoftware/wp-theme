5caee87f04d6cfe2a8f77405d606239f3fd08c42
businessofsoftware <80267145+businessofsoftware@users.noreply.github.com>
2021-05-19 15:21:02 +0100

    move woo styles off-page

--- bln-conference/woocommerce.php
+++ bln-conference/woocommerce.php
@@ -6,64 +6,7 @@
 
 <style type="text/css">
 	
-.product .onsale, .product .woocommerce-product-gallery, .product .woocommerce-tabs, .product .related, .product .product_meta {
-	display: none;
-}
 
-.product-thumbnail {
-	display: none;
-}
-
-.product .price {
-	color: black;
-    margin-top: 20px;
-    font-weight: bold;
-}
-
-.quantity-label {
-	color: #000;
-}
-
-.product del {
-	color: gray;
-}
-
-.product h1 {
-	font-size: 36px;
-}
-
-.product h3 {
-	font-size: 24px;
-}
-
-.product .woocommerce-product-details__short-description {
-	margin-bottom: 20px;
-}
-
-.product input.qty {
-	padding: 6px;
-	font-weight: bold;
-}
-
-.product .wc-box-office-ticket-fields {
-	margin-bottom: 40px;
-}
-
-.product input[type=text], .product input[type=email] {
-	width: 300px;
-}
-
-.woocommerce-notices-wrapper {
-	margin-bottom: 20px;
-}
-
-.woocommerce-notices-wrapper .button {
-	padding: 0.0em 1.0em;
-}
-
-.woocommerce {
-	margin-top: 40px;
-}
 
 </style>
 
