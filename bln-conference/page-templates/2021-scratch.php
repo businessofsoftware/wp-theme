cac59035f55882774eb243c42a2da13a6c953e96
businessofsoftware <80267145+businessofsoftware@users.noreply.github.com>
2021-05-27 08:51:10 +0100

    testing ground template

---
+++ bln-conference/page-templates/2021-scratch.php
@@ -0,0 +1,21 @@
+<?php
+	/*
+	Template Name: 2021: Scratch Pad
+	*/
+    get_header();
+?>
+
+<style type="text/css">
+
+
+</style>
+
+<div class="container-outer-full-width">
+ 
+    
+
+</div>
+
+<?php 
+    get_footer();
+?>
