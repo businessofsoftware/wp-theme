c3d6fda2bef3e4f8419a0b9aa7f603e5f02349e1
businessofsoftware <80267145+businessofsoftware@users.noreply.github.com>
2021-06-25 09:34:47 +0100

    Add optimizescript

--- bln-conference/header.php
+++ bln-conference/header.php
@@ -8,6 +8,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+	<script src="https://www.googleoptimize.com/optimize.js?id=OPT-TCZR68P"></script>
 <meta charset="<?php bloginfo('charset'); ?>">
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri()."/".$cobrand; ?>_favicon.png" />
