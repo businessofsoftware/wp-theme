8d2443dac053950ba549aad15af2a25102218bc1
businessofsoftware <80267145+businessofsoftware@users.noreply.github.com>
2021-07-23 06:34:11 +0100

    swap drip script

--- bln-conference/header.php
+++ bln-conference/header.php
@@ -173,21 +173,7 @@
 		});
 	</script>
 
-	<!-- Drip -->
-	<script type="text/javascript">
-	  var _dcq = _dcq || [];
-	  var _dcs = _dcs || {};
-	  _dcs.account = '6042658';
-
-	  (function() {
-	    var dc = document.createElement('script');
-	    dc.type = 'text/javascript'; dc.async = true;
-	    dc.src = '//tag.getdrip.com/6042658.js';
-	    var s = document.getElementsByTagName('script')[0];
-	    s.parentNode.insertBefore(dc, s);
-	  })();
-	</script>
-	<!-- end Drip -->
+	
 
 
 	<?php do_action('insert_gtm_script'); ?>
