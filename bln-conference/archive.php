4f027f1287c438b0771899fcb727b93caa41a01f
businessofsoftware <80267145+businessofsoftware@users.noreply.github.com>
2021-08-23 12:10:01 +0100

    archive basics

--- bln-conference/archive.php
+++ bln-conference/archive.php
@@ -10,16 +10,19 @@
 	<div class="container-outer bk-white overflow">
 		<div class="container-inner">
 			<div class="content-main">
-				<h1 class="dark"><?php the_archive_title(); ?></h1>
+				<h1 class="dark"><?php single_term_title(); ?></h1>
 
 				<p><?php echo get_the_archive_description(); ?></p>
 
+				<ul>
 				<?php while ( have_posts() ) : ?>
+					
 					<?php the_post(); ?>
 					
-						<h3><a class="blog-title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+					<li><a class="blog-title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
 					
 				<?php endwhile; ?>
+				</ul>
 
 			</div>
 		</div>
